2de3f0532bcbb72643eaf8d69b962a1bfda4b55d
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-06-03 12:12:00 +0530

    Controller layer test cases changes for config server implementation.

--- src/main/java/com/hcl/cloud/product/datatranslator/ViewProductbySkuCodeResponseTranslator.java
+++ src/main/java/com/hcl/cloud/product/datatranslator/ViewProductbySkuCodeResponseTranslator.java
@@ -3,12 +3,12 @@ package com.hcl.cloud.product.datatranslator;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.hcl.cloud.product.config.ConfigLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
+
+import com.hcl.cloud.product.config.ConfigLoader;
 import com.hcl.cloud.product.exception.ProductException;
 import com.hcl.cloud.product.request.CreateproductReq;
 import com.hcl.cloud.product.response.ViewproductRes;
@@ -21,9 +21,8 @@ import com.hcl.cloud.product.response.ViewproductRes;
 public class ViewProductbySkuCodeResponseTranslator {
     static Logger log = LoggerFactory.getLogger(ViewProductbySkuCodeResponseTranslator.class);
 
-    @Autowired
-    private ConfigLoader configLoader;
-    /**
+    
+   /**
      * This method is used as translator from backend to frontend.
      * 
      * @param pList
@@ -33,6 +32,7 @@ public class ViewProductbySkuCodeResponseTranslator {
     public ViewproductRes viewProductbySkuCodeResponseTranslator(List<CreateproductReq> pList, Environment env)
             throws ProductException {
         log.info("Response translation from backend to frontend start");
+        ConfigLoader configLoader = new ConfigLoader();
         ViewproductRes viewproductRes = new ViewproductRes();
         List<CreateproductReq> productsList = new ArrayList<CreateproductReq>();
         for (CreateproductReq products : pList) {
